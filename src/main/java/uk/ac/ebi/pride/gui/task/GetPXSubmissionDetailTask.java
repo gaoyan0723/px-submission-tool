8d34ebc5e2538f0b1d4cf27617a8d7c40dc825d4
Tobias Ternent <tobias@ebi.ac.uk>
2017-06-22 08:42:08 +0100

    New (Mongo) User WS in parallel w/ old (Oracle) User WS. URL set to localhost for now until prod mapping is defined and new User WS released.

--- src/main/java/uk/ac/ebi/pride/gui/task/GetPXSubmissionDetailTask.java
+++ src/main/java/uk/ac/ebi/pride/gui/task/GetPXSubmissionDetailTask.java
@@ -1,16 +1,24 @@
 package uk.ac.ebi.pride.gui.task;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.pride.App;
+import uk.ac.ebi.pride.archive.archiverepomongo.model.ProjectReference;
+import uk.ac.ebi.pride.archive.archiverepomongo.model.User;
+import uk.ac.ebi.pride.archive.dataprovider.person.Title;
 import uk.ac.ebi.pride.archive.submission.model.project.ProjectDetail;
 import uk.ac.ebi.pride.archive.submission.model.project.ProjectDetailList;
+import uk.ac.ebi.pride.archive.submission.model.user.ContactDetail;
 import uk.ac.ebi.pride.gui.util.Constant;
+import uk.ac.ebi.pride.toolsuite.gui.desktop.DesktopContext;
 import uk.ac.ebi.pride.web.util.template.SecureRestTemplateFactory;
 
 import javax.swing.*;
@@ -28,54 +36,81 @@ import java.util.regex.Matcher;
  */
 public class GetPXSubmissionDetailTask extends AbstractWebServiceTask<Set<String>> {
     private static final Logger logger = LoggerFactory.getLogger(GetPXSubmissionDetailTask.class);
-
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+    private String username, password;
 
     public GetPXSubmissionDetailTask(String userName, String password) {
         this.restTemplate = SecureRestTemplateFactory.getTemplate(userName, password);
+        this.username = userName;
+        this.password = password;
     }
 
     @Override
     protected Set<String> doInBackground() throws Exception {
-        Set<String> pxAccessions = new LinkedHashSet<String>();
+        Set<String> pxAccessions = attemptNewUserWsGetAccessions();
+        if (pxAccessions==null) {
+            pxAccessions = new LinkedHashSet<String>();
+            String baseUrl = App.getInstance().getDesktopContext().getProperty("px.submission.detail.url");
+            try {
+                Properties props = System.getProperties();
+                String proxyHost = props.getProperty("http.proxyHost");
+                String proxyPort = props.getProperty("http.proxyPort");
 
-        String baseUrl = App.getInstance().getDesktopContext().getProperty("px.submission.detail.url");
-
-        try {
-            Properties props = System.getProperties();
-            String proxyHost = props.getProperty("http.proxyHost");
-            String proxyPort = props.getProperty("http.proxyPort");
-
-            if (proxyHost != null && proxyPort != null) {
-                HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
-                DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
-                HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
-                defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-            }
-            ProjectDetailList projectDetailList = restTemplate.getForObject(baseUrl, ProjectDetailList.class);
-
-            for (ProjectDetail projectDetail : projectDetailList.getProjectDetails()) {
-                String accession = projectDetail.getAccession();
-                Matcher matcher = Constant.PX_ACC_PATTERN.matcher(accession);
-                if (matcher.matches()) {
-                    pxAccessions.add(accession);
+                if (proxyHost != null && proxyPort != null) {
+                    HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
+                    DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
+                    HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
+                    defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
                 }
+                ProjectDetailList projectDetailList = restTemplate.getForObject(baseUrl, ProjectDetailList.class);
+
+                for (ProjectDetail projectDetail : projectDetailList.getProjectDetails()) {
+                    String accession = projectDetail.getAccession();
+                    Matcher matcher = Constant.PX_ACC_PATTERN.matcher(accession);
+                    if (matcher.matches()) {
+                        pxAccessions.add(accession);
+                    }
+                }
+            } catch (Exception ex) {
+                failedToLogin(ex);
+            }
+        }
+        return pxAccessions;
+    }
+
+    private Set<String> attemptNewUserWsGetAccessions() {
+        Set<String> pxAccessions = null;
+        try {
+            User user = GetPrideUserDetailTask.loginUser(username, password);
+            if (user != null && !StringUtils.isEmpty(user.getEmail())) {
+                pxAccessions = new LinkedHashSet<>();
+                if (user.getProjectReferences() != null && user.getProjectReferences().length > 0) {
+                    for (ProjectReference projectReference : user.getProjectReferences()) {
+                        Matcher matcher = Constant.PX_ACC_PATTERN.matcher(projectReference.getAccession());
+                        if (matcher.matches()) {
+                            pxAccessions.add(projectReference.getAccession());
+                        }
+                    }
+                }
+            } else {
+                throw new Exception();
             }
         } catch (Exception ex) {
-            logger.warn("Failed to login to retrieve project details", ex);
-            Runnable eventDispatcher = new Runnable() {
-                public void run() {
-                    // show warning dialog
-                    App app = (App) App.getInstance();
-                    JOptionPane.showConfirmDialog(app.getMainFrame(),
-                            app.getDesktopContext().getProperty("pride.login.resubmission.error.message"),
-                            app.getDesktopContext().getProperty("pride.login.error.title"),
-                            JOptionPane.CLOSED_OPTION, JOptionPane.WARNING_MESSAGE);
-                }
-            };
-            EventQueue.invokeLater(eventDispatcher);
+            // failedToLogin(ex);
+            // do nothing for now, until migrated fully to new User WS
         }
-
         return pxAccessions;
+    }
+
+    private void failedToLogin(Exception ex) {
+        logger.warn("Failed to login to retrieve project details", ex);
+        Runnable eventDispatcher = () -> {
+            App app = (App) App.getInstance(); // show warning dialog
+            JOptionPane.showConfirmDialog(app.getMainFrame(),
+                app.getDesktopContext().getProperty("pride.login.resubmission.error.message"),
+                app.getDesktopContext().getProperty("pride.login.error.title"),
+                JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE);
+        };
+        EventQueue.invokeLater(eventDispatcher);
     }
 }
--- src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
+++ src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
@@ -1,18 +1,26 @@
 package uk.ac.ebi.pride.gui.task;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.pride.App;
+import uk.ac.ebi.pride.archive.archiverepomongo.model.User;
+import uk.ac.ebi.pride.archive.dataprovider.person.Title;
 import uk.ac.ebi.pride.archive.submission.model.user.ContactDetail;
 import uk.ac.ebi.pride.toolsuite.gui.desktop.DesktopContext;
 import uk.ac.ebi.pride.toolsuite.gui.task.TaskAdapter;
 import uk.ac.ebi.pride.web.util.template.SecureRestTemplateFactory;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 /**
@@ -25,7 +33,9 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(GetPrideUserDetailTask.class);
 
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+    private String username;
+    private String password;
 
     /**
      * Constructor
@@ -35,6 +45,8 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
      */
     public GetPrideUserDetailTask(String userName, char[] password) {
         this.restTemplate = SecureRestTemplateFactory.getTemplate(userName, new String(password));
+        this.username = userName;
+        this.password = String.valueOf(password);
     }
 
     /**
@@ -51,29 +63,70 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
      */
     @Override
     protected ContactDetail doInBackground() throws Exception {
-        DesktopContext context = App.getInstance().getDesktopContext();
-        String baseUrl = context.getProperty("pride.user.detail.url");
-
-        try {
-            // set proxy
-            Properties props = System.getProperties();
-            String proxyHost = props.getProperty("http.proxyHost");
-            String proxyPort = props.getProperty("http.proxyPort");
-
-            if (proxyHost != null && proxyPort != null) {
-                logger.info("Using proxy server {} and port {}", proxyHost, proxyPort);
-                HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
-                DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
-                HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
-                defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+        ContactDetail result = attemptNewUserWsLogin();
+        if (result==null) { // old User WS
+            DesktopContext context = App.getInstance().getDesktopContext();
+            String baseUrl = context.getProperty("pride.user.detail.url");
+            try {
+                // set proxy
+                Properties props = System.getProperties();
+                String proxyHost = props.getProperty("http.proxyHost");
+                String proxyPort = props.getProperty("http.proxyPort");
+                if (proxyHost != null && proxyPort != null) {
+                    logger.info("Using proxy server {} and port {}", proxyHost, proxyPort);
+                    HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
+                    DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
+                    HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
+                    defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+                }
+                return restTemplate.getForObject(baseUrl, ContactDetail.class);
+            } catch (Exception ex) {
+                publish("Failed to login, please check user name or password");
             }
-
-            return restTemplate.getForObject(baseUrl, ContactDetail.class);
-        } catch (Exception ex) {
-            publish("Failed to login, please check user name or password");
         }
+        return result;
+    }
 
-        return null;
+    private ContactDetail attemptNewUserWsLogin() {
+        ContactDetail result = null;
+        try {
+            User user = loginUser(username, password);
+            if (user!=null && !StringUtils.isEmpty(user.getEmail())) {
+                result = new ContactDetail();
+                result.setEmail(user.getEmail());
+                result.setAffiliation(user.getAffiliation());
+                result.setTitle(Title.fromString(user.getTitle()));
+                result.setFirstName(user.getFirstName());
+                result.setLastName(user.getSecondName());
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception ex) {
+            // publish("Failed to login, please check user name or password");
+            // do nothing for now, until migrated fully to new User WS
+        }
+        return result;
+    }
+
+    static User loginUser(String username, String password) throws Exception{
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+        Properties props = System.getProperties(); // set proxy
+        String proxyHost = props.getProperty("http.proxyHost");
+        String proxyPort = props.getProperty("http.proxyPort");
+        if (proxyHost != null && proxyPort != null) {
+            logger.info("Using proxy server {} and port {}", proxyHost, proxyPort);
+            HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
+            DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
+            HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
+            defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+        }
+        User loginUser = new User();
+        loginUser.setEmail(username);
+        loginUser.setPassword(password);
+        return restTemplate.postForObject(
+            App.getInstance().getDesktopContext().getProperty("pride.user.detail.url.new") + "/loginuser",
+            new HttpEntity<>(loginUser),
+            User.class);
     }
 }
 
